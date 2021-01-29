102c57a990d5e453fe7fa2c4aa820e854d69b7b0
diego <diego@DESKTOP-QGJ6HOR>
2021-01-29 11:48:37 +0100

    add some features to the programm

--- src/socketbase/ServerBase.java
+++ src/socketbase/ServerBase.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 package socketbase;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.*;
@@ -47,11 +50,19 @@ public class ServerBase {
                 System.out.println("Un cliente connesso!!!");
                 
                 OutputStream versoIlClient = client.getOutputStream();
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(versoIlClient));
+                BufferedWriter scrittore = new BufferedWriter(new OutputStreamWriter(versoIlClient));
+                InputStream dalClient = client.getInputStream();
+                BufferedReader lettore = new BufferedReader(new InputStreamReader(dalClient));
                 
-                bw.write("Ciao bello sono il server");
+                scrittore.write("Ciao bello sono il server");
+                String risposta = lettore.readLine();
+                System.out.println("risposta del client: "+risposta);
+                scrittore.write("Come va la vita?");
+                risposta = lettore.readLine();
+                System.out.println("risposta del client: "+risposta);
                 
-                bw.close();
+                scrittore.close();
+                lettore.close();
                 client.close();
                 
                 System.out.println("Chiusura connessione effettuata");
